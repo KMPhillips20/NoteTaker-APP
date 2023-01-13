570fa5a8b420b2ffc630696fc70e895ba556cd49
Kyle <kylemphillips65@yahoo.com>
2023-01-12 19:34:43 -0500

    still working

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static('public'));
 
 
-app.get('/', (rec, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 app.listen(PORT, () => console.log('runing at port 3001'));
