8f3c95fceb853284fc08cccfe7ab06e4f4c9f068
Kyle <kylemphillips65@yahoo.com>
2023-01-17 16:33:00 -0500

    added api routes for notes info

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
+
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 const PORT = process.env.PORT || 3001;
-
+const path = require('path');
+const {v4: uuid4} = require('uuid');
 const app = express();
 
+
+// Middle wear
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 
+app.get('/api/notes', (req,res) => {
+  fs.readFileSync('./db/db.json', 'utf8');
+  const notesInfo = JSON.parse(data);
+  res.json(notesInfo);
+});
+
+app.post('/api/notes', (req,res) => {
+  fs.readFileSync('./db/db.json', 'utf8');
+  const notesInfo = JSON.parse(data);
+
+
+  res.json(notesInfo);
+});
+
+// Routes 
+// Path for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Path for Notes 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 
-app.listen(PORT, () => console.log('runing at port 3001'));
+
+
+app.listen(PORT, () => console.log('Port 3001 is running'));
